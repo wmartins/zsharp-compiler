f10b9e3fe2abbc1133aee3864188cc40f9db95be
Italo Qualisoni <italo.qualisoni@acad.pucrs.br>
2013-10-27 19:06:27 -0200

    Updated the semantic adding %left and %nonassoc

--- src/semantic/Symbol.java
+++ src/semantic/Symbol.java
@@ -33,23 +33,42 @@ public class Symbol {
 		return s;
 	}
 
+	public String translateName(Kinds kind, String name) {
+		switch(kind) {
+			case Local:
+				return name + " - local";
+			case Meth:
+				return name + " - meth";
+			default:
+				return name;
+		}
+	}
+
 	public Symbol addMethod(String type, String name) {
+		String newName = translateName(Kinds.Local, name);
+		if(locals.get(newName) != null) {
+			System.out.println("Método já declarado :/");
+		}
 		Symbol s = new Symbol();
-		s.name = name;
+		s.name = newName;
 		s.kind = Kinds.Meth;
 		s.type = type;
 
-		locals.put(name, s);
+		locals.put(newName, s);
 		return s;
 	}
 
 	public Symbol addLocal(String type, String name) {
+		String newName = translateName(Kinds.Local, name);
+		if(locals.get(newName) != null) {
+			System.out.println("Variável já declarada :/");
+		}
 		Symbol s = new Symbol();
-		s.name = name;
+		s.name = newName;
 		s.kind = Kinds.Local;
 		s.type = type;
 
-		locals.put(name, s);
+		locals.put(newName, s);
 		return s;
 	}
 
